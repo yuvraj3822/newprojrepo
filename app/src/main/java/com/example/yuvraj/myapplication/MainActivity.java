48540cc78cd86a066c41dde67e55dac81f3265af
Yuvraj Sharma <yuvraj.sharma@codroit.com>
2017-07-15 22:58:10 +0530

    text view pending

--- app/src/main/java/com/example/yuvraj/myapplication/MainActivity.java
+++ app/src/main/java/com/example/yuvraj/myapplication/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.yuvraj.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView aaldlmlknm;
+    EditText lohjaofdlkj;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
